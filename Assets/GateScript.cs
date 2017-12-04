300cbf66daac4ef9a8f177be7af24076a456b549
Leifur16 <leifur16@ru.is>
2017-12-04 13:56:40 +0000

    press button and open gate

---
+++ Assets/GateScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateScript : MonoBehaviour {
+
+	Animator anim;
+
+	// Use this for initialization
+	void Start()
+	{
+		anim = GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	public void OpenGate()
+	{
+		anim.SetBool("Open", true);
+	}
+
+	public void CloseGate()
+	{
+		anim.SetBool("Open", false);
+		Debug.Log("dsadas");
+	}
+}
