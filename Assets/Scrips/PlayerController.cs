e4701f4283120d0f5b686890ccc335f99f8f17f2
Carlos RAgnar <carlosk12@ru.is>
2017-12-04 19:07:33 +0000

    Movable player with gravity

---
+++ Assets/Scrips/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody2D rb2d;
+    private GameObject upDownColBox;
+    private GameObject leftRightColBox;
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+
+        Physics2D.gravity = new Vector3(0, -9.81F, 0);
+
+
+        transform.GetChild(1).gameObject.SetActive(false);
+        transform.GetChild(2).gameObject.SetActive(true);
+
+        //rb2d.freezeRotation = true;
+    }
+	
+    void Update() {
+    }
+
+    void FixedUpdate () {
+        float moveHor = Input.GetAxis("Horizontal");
+        float moveVer = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHor * speed, moveVer * speed);
+
+        rb2d.AddForce(movement);
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            print("A");
+            Physics2D.gravity = new Vector3(-9.81F, 0, 0);
+            transform.GetChild(1).gameObject.SetActive(true);
+            transform.GetChild(2).gameObject.SetActive(false);
+            //rb2d.MoveRotation(-90);
+        }
+        else if(Input.GetKeyDown(KeyCode.S))
+        {
+            print("S");
+            Physics2D.gravity = new Vector3(0, -9.81F, 0);
+            transform.GetChild(1).gameObject.SetActive(false);
+            transform.GetChild(2).gameObject.SetActive(true);
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            print("W");
+            Physics2D.gravity = new Vector3(0, 9.81F, 0);
+            transform.GetChild(1).gameObject.SetActive(false);
+            transform.GetChild(2).gameObject.SetActive(true);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            print("D");
+            Physics2D.gravity = new Vector3(9.81F, 0, 0);
+            transform.GetChild(1).gameObject.SetActive(true);
+            transform.GetChild(2).gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scrips/mouseSprite.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseSprite : MonoBehaviour {
+
+    public Sprite[] sprites;
+    private SpriteRenderer renderer;
+
+    // Use this for initialization
+    void Start () {
+        renderer = gameObject.GetComponent<SpriteRenderer>();
+        renderer.sprite = sprites[0];
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(Physics2D.gravity.x == 0)
+        {
+            if(Physics2D.gravity.y < 0)
+            {
+                renderer.sprite = sprites[0];
+            }
+            else if(Physics2D.gravity.y > 0)
+            {
+                renderer.sprite = sprites[1];
+            }
+        }
+        else if(Physics2D.gravity.y == 0)
+        {
+            if(Physics2D.gravity.x < 0)
+            {
+                renderer.sprite = sprites[2];
+            }
+            else if(Physics2D.gravity.x > 0)
+            {
+                renderer.sprite = sprites[3];
+            }
+        }
+        //if (Input.GetKeyDown(KeyCode.A))
+        //{
+        //    renderer.sprite = sprites[2];
+        //}
+        //else if (Input.GetKeyDown(KeyCode.S))
+        //{
+        //    renderer.sprite = sprites[0];
+        //}
+        //else if (Input.GetKeyDown(KeyCode.W))
+        //{
+        //}
+        //else if (Input.GetKeyDown(KeyCode.D))
+        //{
+        //    renderer.sprite = sprites[3];
+        //}
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            renderer.sprite = sprites[2];
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            renderer.sprite = sprites[0];
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            renderer.sprite = sprites[1];
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            renderer.sprite = sprites[3];
+        }
+    }
+}
