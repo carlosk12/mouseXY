909894f0083dce506274e7cea6dc4de44ed33057
Carlos RAgnar <carlosk12@ru.is>
2017-12-05 16:01:04 +0000

    Changes to player Controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,7 +9,7 @@ public class PlayerController : MonoBehaviour {
     private GameObject upDownColBox;
     private GameObject leftRightColBox;
     private bool isUpOrDown = true;
-    //private bool inAir = false;
+    private bool inAir = false;
 
     void Awake()
     {
@@ -38,28 +38,14 @@ public class PlayerController : MonoBehaviour {
             PlayerMoveLR();
         }
 
-        if(Input.GetKeyUp(KeyCode.Space))
+        if(Input.GetKeyUp(KeyCode.Space) && !inAir)
         {
             //gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, speed);
-            rb2d.velocity = new Vector2(0, 5);
-            //inAir = true;
+            //rb2d.velocity = new Vector2(0, 5);
+            inAir = true;
+            rb2d.AddForce(new Vector2(0, 300));
         }
         
-        //Vector2 movement = new Vector2(speed, 0);
-
-        //rb2d.AddForce(movement);
-        //gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveHor * speed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
-
-        //if (Input.GetKeyDown(KeyCode.LeftArrow))
-        //{
-        //    //rb2d.AddForce(new Vector2(speed, 0));
-        //    gameObject.GetComponent<Rigidbody2D>().velocity = -movement;
-        //}
-        //else if (Input.GetKeyDown(KeyCode.RightArrow))
-        //{
-        //    //rb2d.AddForce(new Vector2(speed, 0));
-        //    gameObject.GetComponent<Rigidbody2D>().velocity = movement;
-        //}
 
         if (Input.GetKeyDown(KeyCode.A))
         {
@@ -94,27 +80,6 @@ public class PlayerController : MonoBehaviour {
             transform.GetChild(2).gameObject.SetActive(false);
             isUpOrDown = false;
         }
-
-        //if (Input.GetKeyUp(KeyCode.LeftArrow))
-        //{
-        //    rb2d.velocity = Vector2.zero;
-        //    rb2d.angularVelocity = 0;
-        //}
-        //else if (Input.GetKeyUp(KeyCode.DownArrow))
-        //{
-        //    rb2d.velocity = Vector2.zero;
-        //    rb2d.angularVelocity = 0;
-        //}
-        //else if (Input.GetKeyUp(KeyCode.UpArrow))
-        //{
-        //    rb2d.velocity = Vector2.zero;
-        //    rb2d.angularVelocity = 0;
-        //}
-        //else if (Input.GetKeyUp(KeyCode.RightArrow))
-        //{
-        //    rb2d.velocity = Vector2.zero;
-        //    rb2d.angularVelocity = 0;
-        //}
     }
 
     void PlayerMoveUD()
@@ -141,6 +106,11 @@ public class PlayerController : MonoBehaviour {
         if(col.gameObject.name == "CratePink")
         {
             col.gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+        }
+
+        if(col.gameObject.transform.position.y < gameObject.transform.position.y)
+        {
+            inAir = false;
         }
     }
 
