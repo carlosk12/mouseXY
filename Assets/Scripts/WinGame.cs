c33fed36d448116ea5c3ea34df75adb51c17a4d6
Leifur16 <leifur16@ru.is>
2017-12-07 17:47:08 +0000

    Added text mesh pro

---
+++ Assets/Scripts/WinGame.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class WinGame : MonoBehaviour {
+
+	public TextMeshPro winText;
+
+	private void Awake()
+	{
+		winText.gameObject.SetActive(false);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.gameObject.tag == "Player")
+		{
+			winText.gameObject.SetActive(true);
+		}
+	}
+}
