0decf99fb61013bca5058f6cce258510f1160678
Leifur16 <leifur16@ru.is>
2017-12-09 15:25:00 +0000

    working on audiomanager

--- Assets/Scripts/Sound.cs
+++ Assets/Scripts/Sound.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class Sound : MonoBehaviour {
+public class Sound {
+	public string name;
 
 	public AudioClip clip;
 
+	[Range(0f, 1f)]
 	public float volume;
+	[Range(.1f, 3f)]
 	public float pitch;
+
+	[HideInInspector]
+	public AudioSource source;
 }
