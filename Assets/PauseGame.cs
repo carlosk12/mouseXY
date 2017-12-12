df09298cc9726c5a115335303d5ad616836985e4
Carlos RAgnar <carlosk12@ru.is>
2017-12-12 11:33:12 +0000

    New Background and main menu UI along with a Pause menu in mainscene

---
+++ Assets/PauseGame.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour {
+
+	void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(Time.timeScale != 0)
+            {
+                Time.timeScale = 0;
+                gameObject.SetActive(true);
+            }
+            else
+            {
+                Time.timeScale = 1;
+                gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public void UnpauseGame()
+    {
+        Time.timeScale = 1;
+        gameObject.SetActive(false);
+    }
+}
