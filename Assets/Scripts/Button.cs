e35ffef4c04edc542cfcddaf19f590933dcd5ad7
bjarki15 <bjarki15@ru.is>
2017-12-13 13:23:36 +0000

    UI changes

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -23,7 +23,7 @@ public class Button : MonoBehaviour {
 	void OnTriggerEnter2D() {
 		anim.SetBool("GoDown", true);
         
-i
+
         foreach (GateTrigger i in gateTrig){
         	i.Toggle(true);
         }
